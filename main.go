fafbbc978d67bb70b30fa4b73b6df90a3702def1
Hari Prediansyah <coyhari9@gmail.com>
2024-05-27 11:29:16 +0700

    changed

--- main.go
+++ main.go
@@ -4,9 +4,22 @@ import "fmt"
 
 func main() {
 	fmt.Println("Test")
-	fmt.Println(fmt.Sprintf("1 + 2 = %d", calc(1, 2)))
+	var a = 2
+	var b = 3
+	fmt.Println(fmt.Sprintf("%d + %d = %d", a, b, calc(a, b, 1)))
+	fmt.Println(fmt.Sprintf("%d * %d = %d", a, b, calc(a, b, 3)))
 }
 
-func calc(a int, b int) int {
-	return a + b
+func calc(a int, b int, tipe int) int {
+	// 1: tambah, 2: kurang, 3: kali, 4: bagi
+	switch tipe {
+	case 1:
+		return a + b
+	case 2:
+		return a - b
+	case 3:
+		return a * b
+	default:
+		return a / b
+	}
 }
